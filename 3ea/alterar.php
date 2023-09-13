e85c214589b248c79f8b438b4585720612c3b992
Luísa Vitória <luisa.vitoria@hotmail.com>
2023-09-13 19:24:02 -0300

    add 3ea

---
+++ 3ea/alterar.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Alteração Cadastro</title>
+</head>
+
+<body>
+
+    <?php
+    require_once('connect.php');
+
+    if (isset($_POST['save'])) {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $age = $_POST['age'];
+        $local = $_POST['local'];
+        $id = $_POST['id'];
+
+        if (empty($id)) {
+            $msg_id = "Campo obrigatório!";
+        }
+
+        if (empty($name)) {
+            $msg_name = "Campo obrigatório!";
+        }
+
+        if (empty($phone)) {
+            $msg_phone = "Campo obrigatório!";
+        }
+
+        if (empty($age)) {
+            $msg_age = "Campo obrigatório!";
+        }
+
+        if (empty($local)) {
+            $msg_local = "Campo obrigatório!";
+        }
+
+        if (!empty($name) && !empty($phone) && !empty($age) && !empty($local)) {
+            $sql = "SELECT * FROM moradia WHERE idmoradia='$id'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $sql = "UPDATE moradia SET nome='$name', telefone='$phone', idade='$age', local='$local' WHERE idmoradia='$id'";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Cadastro alterado com sucesso!";
+                }
+            } else {
+                echo "Id não encontrado no Banco de Dados.";
+            }
+        }
+    }
+    ?>
+
+    <h1>Alterar Cadastro</h1>
+
+    <form method="post" action="">
+        <div class="container">
+            <label>Id</label>
+            <input type="number" name="id" placeholder="1" />
+            <?php if (isset($msg_id)) echo "<p class='error'>" . $msg_id . "</p>"; ?>
+        </div>
+
+        <div class="container">
+            <label>Nome</label>
+            <input type="text" name="name" placeholder="José Santos" />
+            <?php if (isset($msg_name)) echo "<p class='error'>" . $msg_name . "</p>"; ?>
+        </div>
+
+        <div class="container">
+            <label>Telefone</label>
+            <input type="tel" name="phone" placeholder="(82) 99999-8888" />
+            <?php if (isset($msg_phone)) echo "<p class='error'>" . $msg_phone . "</p>"; ?>
+        </div>
+
+        <div class="container">
+            <label>Idade</label>
+            <input type="number" name="age" min="0" max="150" placeholder="50" />
+            <?php if (isset($msg_age)) echo "<p class='error'>" . $msg_age . "</p>"; ?>
+        </div>
+
+        <div class="container">
+            <label>Local</label>
+            <input type="text" name="local" placeholder="Maceió" />
+            <?php if (isset($msg_local)) echo "<p class='error'>" . $msg_local . "</p>"; ?>
+        </div>
+
+        <button type="submit" class="button-save" name="save">Alterar</button>
+    </form>
+</body>
+
+</html>
---
+++ 3ea/connect.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$database = "web";
+$user = "root";
+$password = "sqlpassword";
+
+$conn = new mysqli($server, $user, $password, $database);
+?>
